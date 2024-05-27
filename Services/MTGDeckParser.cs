7eb39ad21efa443e9e147cc9bf55316faf722fbb
DawnDevelop <32778430+DawnDevelop@users.noreply.github.com>
2024-05-27 12:41:33 +0200

    returned layout

--- Services/MTGDeckParser.cs
+++ Services/MTGDeckParser.cs
@@ -6,133 +6,105 @@ using MTGAWeightsCalculator.Models;
 
 namespace MTGAWeightsCalculator.Services;
 
-
 public partial class MTGDeckParser(HttpClient httpClient)
 {
+    private readonly HttpClient _httpClient = httpClient;
+    private Dictionary<string, WeightedCard>? _mainDeckWeightsCache;
+    private Dictionary<string, int>? _commanderWeightsCache;
+    private static readonly char[] separator = ['\r', '\n'];
 
     public async Task<OutputCards?> ParseDeck(string cards)
     {
+        if (_mainDeckWeightsCache == null || _commanderWeightsCache == null)
+            await InitializeAsync();
+
         var inputCards = ParseDeckFromInput(cards);
 
-        var weightedCards = await GetMainDeckCardWeights();
-
-        if (weightedCards == null)
-            return null;
-
         var totalWeight = 0;
-
         var outputCards = new List<OutputCard>();
+
         foreach (var inputCard in inputCards)
         {
-            if(inputCard.IsCommander)
+            if (inputCard.IsCommander)
             {
-                var commanderWeight = await GetCommanderWeight(inputCard.Name);
+                var commanderWeight = await GetCommanderWeightAsync(inputCard.Name);
                 totalWeight += commanderWeight;
                 outputCards.Add(new OutputCard(inputCard.Quantity, inputCard.Name, commanderWeight));
             }
-            else if (weightedCards.TryGetValue(inputCard.Name, out var weightedCard))
+            else if (_mainDeckWeightsCache!.TryGetValue(inputCard.Name, out var weightedCard))
             {
-                outputCards.Add(new OutputCard(inputCard.Quantity, inputCard.Name, weightedCard.Weight));
-                totalWeight += weightedCard.Weight * inputCard.Quantity;
+                var cardWeight = weightedCard.Weight * inputCard.Quantity;
+                outputCards.Add(new OutputCard(inputCard.Quantity, inputCard.Name, cardWeight));
+                totalWeight += cardWeight;
             }
         }
-        
+
         return new OutputCards(totalWeight, outputCards);
     }
 
-    public async Task<Dictionary<string, WeightedCard>> GetMainDeckCardWeights()
+    public async Task InitializeAsync()
     {
-        var weightedCards = new Dictionary<string, WeightedCard>();
-
-        var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
-        {
-            PrepareHeaderForMatch = args => args.Header.ToLower()
-        };
-
-        var csvContent = await httpClient.GetStringAsync("csv/WeightsMainDeck.csv");
-
-        using var reader = new StringReader(csvContent);
-        using var csv = new CsvReader(reader, csvConfig);
-        
-        var records = csv.GetRecordsAsync<WeightedCard>();
-
-        await foreach (var card in records)
-        {
-            weightedCards[card.Name] = card;
-        }
-        
-        return weightedCards;
+        await Task.WhenAll(InitializeMainDeckWeightsCache(), InitializeCommanderWeightsCache());
     }
 
-    public async Task<int> GetSingleCardWeight(string cardName)
+    public async Task InitializeMainDeckWeightsCache()
     {
-        var weights = await GetMainDeckCardWeights();
+        var csvContent = await _httpClient.GetStringAsync("csv/WeightsMainDeck.csv");
+        _mainDeckWeightsCache = ParseCsv<string, WeightedCard>(csvContent, dis => dis.Name, card => card.Name);
+    }
 
-        if (weights.TryGetValue(cardName, out var card))
+    public async Task InitializeCommanderWeightsCache()
+    {
+        var csvContent = await _httpClient.GetStringAsync("csv/WeightsCommander.csv");
+        _commanderWeightsCache = ParseCsv<string, WeightedCard>(csvContent, dis => dis.Name, card => card.Name)
+            .ToDictionary(card => card.Key, card => card.Value.Weight);
+    }
+
+    public async Task<int> GetCommanderWeightAsync(string cardName)
+    {
+        if (_commanderWeightsCache == null)
+            await InitializeCommanderWeightsCache();
+
+        return _commanderWeightsCache!.TryGetValue(cardName, out var weight) ? weight : 0;
+    }
+    public async Task<int> GetSingleCardWeightAsync(string cardName)
+    {
+        if(_mainDeckWeightsCache == null)
+            await InitializeMainDeckWeightsCache();
+
+        if (_mainDeckWeightsCache!.TryGetValue(cardName, out var card))
             return card.Weight;
-        else 
+        else
+        {
             return 0;
-    }
-
-    public async Task<int> GetCommanderWeight(string cardName)
-    {
-
-        var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
-        {
-            PrepareHeaderForMatch = args => args.Header.ToLower()
-        };
-
-        var csvContent = await httpClient.GetStringAsync("csv/WeightsCommander.csv");
-        using var reader = new StringReader(csvContent);
-        using var csv = new CsvReader(reader, csvConfig);
-        var records = csv.GetRecords<WeightedCard>();
-
-        foreach (var card in records)
-        {
-            if (card.Name == cardName)
-                return card.Weight;
         }
-
-        return 0;
     }
 
-    public List<InputCard> ParseDeckFromInput(string input)
+    public static List<InputCard> ParseDeckFromInput(string input)
     {
         var cards = new List<InputCard>();
         var isCommanderSection = false;
-        var lines = input.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-
+        var lines = input.Split(separator, StringSplitOptions.RemoveEmptyEntries);
 
         foreach (var line in lines)
         {
             var newLine = RemoveExtraInfo(line);
-
-            if (newLine.StartsWith("Deck"))
-                continue;
-            if (newLine.StartsWith("Sideboard"))
+            if (newLine.StartsWith("Deck") || newLine.StartsWith("Sideboard"))
                 continue;
 
             if (newLine.StartsWith("Commander"))
             {
                 isCommanderSection = true;
-                continue; 
+                continue;
             }
 
             var parts = newLine.Split(' ', 2);
             if (int.TryParse(parts[0], out int quantity))
             {
-                if(isCommanderSection)
-                {
-                    cards.Add(new InputCard(parts[1], 1, true)); // Mark as commander card
-                    isCommanderSection = false;
-                    continue;
-                }
-                else
-                {
-                    cards.Add(new InputCard(parts[1], quantity));
-                }
+                var isCommander = isCommanderSection;
+                cards.Add(new InputCard(parts[1], isCommander ? 1 : quantity, isCommander));
+                isCommanderSection = false;
             }
-
         }
 
         return cards;
@@ -140,8 +112,27 @@ public partial class MTGDeckParser(HttpClient httpClient)
 
     public static string RemoveExtraInfo(string input)
     {
-        string pattern = @"\(.*?\).*";
-        var replaced = Regex.Replace(input, pattern, "").Trim();
-        return replaced;
+        return CompiledRegex().Replace(input, "").Trim();
     }
+
+    private static Dictionary<TKey, TValue> ParseCsv<TKey, TValue>(string csvContent, Func<TValue, TKey> distinctBy, Func<TValue, TKey> keySelector)
+        where TKey : notnull
+        where TValue : class
+    {
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            PrepareHeaderForMatch = args => args.Header.ToLower()
+        };
+
+        using var reader = new StringReader(csvContent);
+        using var csv = new CsvReader(reader, config);
+
+        var records = csv.GetRecords<TValue>();
+        var dictionary = records.DistinctBy(distinctBy).ToDictionary(keySelector);
+
+        return dictionary;
+    }
+
+    [GeneratedRegex(@"\(.*?\).*")]
+    private static partial Regex CompiledRegex();
 }
